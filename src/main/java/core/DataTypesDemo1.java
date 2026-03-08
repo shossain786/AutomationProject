5ce2a70db30ffae2e33cc792f4644b7dcd3f42ca
Mohammad Saddam Hossain <sddmhossain786@gmail.com>
2026-03-08 20:50:59 +0530

    data types explained with demo

---
+++ src/main/java/core/DataTypesDemo1.java
@@ -0,0 +1,11 @@
+package core;
+
+public class DataTypesDemo1 {
+    static void main() {
+        byte a  = 10; //range of a byte type variable -128 to 127
+        byte c = -128;
+//        byte b1 = 128; //not acceptable as out of range for byte
+//        short s = -32769;
+    }
+}
+// person age : 127
---
+++ src/main/java/core/TypeCastingDemo1.java
@@ -0,0 +1,19 @@
+package core;
+
+public class TypeCastingDemo1 {
+    static void main() {
+//        byte b = 100;
+//        int i = b;  // implicit type casting
+//        double d = b;
+//        System.out.println("Value of i is: " + i);
+//        System.out.println("Value of d is: " + d);
+
+//        double d = 3.14;
+//        int i = (int) d; // explicit type casting
+//        System.out.println("The value is: " + i);
+
+        int a  = 130;
+        byte b = (byte) a;
+        System.out.println("The value of b is " + b);
+    }
+}
